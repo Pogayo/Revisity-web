eb67987ab8475d6a47cee4216b58074e21aea208
Pogayo <pogayo17@alustudent.com>
2019-03-25 17:28:45 +0200

    Modified the prog-quiz.html by adding more css and questions

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ var q1Div=document.getElementById("q1");
 var q1Explanation=document.createElement('p');
  q1Explanation.innerHTML="The correct answer is D";
  q1Explanation.style.backgroundColor="green";
- q1Div.appendChild(q1Explanation);
+ //q1Div.appendChild(q1Explanation);
 
 
